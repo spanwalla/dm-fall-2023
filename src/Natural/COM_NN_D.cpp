ee00d4d0d66036b7b977057a07b5937ce7873035
Chepasov Dima <Zorinal133@yandex.ru>
2023-10-28 09:53:56 +0300

    refix COM_NN_D add file

--- src/Natural/COM_NN_D.cpp
+++ src/Natural/COM_NN_D.cpp
@@ -1,19 +1,20 @@
-// compare two digits
-#include "NATURAL.h"
+//Created by Chepasov Dmitry 27.10.23 Group:2382
+//Compare two digits
+
 int Natural::COM_NN_D(Natural& cmp){
-        std::vector <short> vcmp = cmp.get_digit();
-        if(vcmp.size() == this->digits.size()){
-            for (int i = vcmp.size()-1;i>=0;i++){
-                if(vcmp[i] != this->digits[i]){
-                    if(vcmp[i]>this->digits[i]){
+        if(cmp.digits.size() == this->digits.size()){
+            for (int i = cmp.digits.size()-1;i>=0;i--){
+                if(cmp.digits[i] != this->digits[i]){
+                    if(cmp.digits[i]>this->digits[i]){
                         return 1;
                     }
-                    if(vcmp[i] <this->digits[i]){
+                    if(cmp.digits[i] <this->digits[i]){
                         return 2;
                     }
                     }
                 }
                 return 0;
             }
-        return (this->digits.size() > vcmp.size() ? 2 : 1);
+        return (this->digits.size() > cmp.digits.size() ? 2 : 1);
+
 }
