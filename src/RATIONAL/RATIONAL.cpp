decff40defe950ce8b54453bf6006ccdcb1753b4
mishIgr <fmikhailv@gmail.com>
2023-10-31 22:13:40 +0300

    RATIONAL

--- src/RATIONAL/RATIONAL.cpp
+++ src/RATIONAL/RATIONAL.cpp
@@ -9,6 +9,8 @@ Rational::Rational(const std::string& number) {
         throw std::invalid_argument("Characted '/' not found.");
     this->numerator = Integer(number.substr(0, number.find('/')));
     this->denominator = Natural(number.substr(number.find('/') + 1));
+    if (!this->denominator.ABS_Z_N().NZER_N_B())
+        throw std::invalid_argument("The denominator cannot be zero.");
 }
 
 Rational::Rational() : numerator("0"), denominator("1") {}
