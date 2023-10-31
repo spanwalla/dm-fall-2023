d215a2385f6d7e1cca49342afff7e5bf15c59cfa
Sorkan176 <ansemenenko176@gmail.com>
2023-10-31 19:29:35 +0300

    TRANS_N_Z_comments_fix

--- src/Integer/TRANS_N_Z.cpp
+++ src/Integer/TRANS_N_Z.cpp
@@ -1,6 +1,5 @@
 // Created by Semenenko Anna 31.10.2023
 // Преобразование натурального в целое
-// static метод, чтобы не создавать временный объект при вызове
 // Integer new_integer_obj = Integer::TRANS_N_Z(nat_obj);
 
 #include "INTEGER.h"
