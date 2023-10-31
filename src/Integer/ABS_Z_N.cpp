6d7f0b986cfce10f672b0ead6761e6ec39656c50
Copybarasad <kirpichotsutstvuet@gmail.com>
2023-10-31 19:42:57 +0300

    ABS_Z_N

---
+++ src/Integer/ABS_Z_N.cpp
@@ -0,0 +1,8 @@
+// Created by Shegoleva Nadezda gr2382 31.10
+// Функция находит абсолютную величину числа
+#include "INTEGER.h"
+
+Natural Integer::ABS_Z_N(const Integer& integer) {
+    Natural absValue = integer.number;
+    return absValue;
+}
