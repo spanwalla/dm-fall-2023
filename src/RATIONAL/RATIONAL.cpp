7b5706175dbbf21dceecb8099afb5f5534503b0e
mishIgr <fmikhailv@gmail.com>
2023-10-31 15:22:54 +0300

    RATIONAL

---
+++ src/RATIONAL/RATIONAL.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Mihail Fedorov (2382) on 31.10.2023.
+//
+
+#include "RATIONAL.h"
+
+Rational::Rational(const std::string& number) {
+    if (number.find('/') == std::string::npos)
+        throw std::invalid_argument("Characted '/' not found.");
+    this->numerator = Integer(number.substr(0, number.find('/')));
+    this->denominator = Natural(number.substr(number.find('/') + 1));
+}
+
+Rational::Rational() : numerator("0"), denominator("1") {}
+
+std::ostream& operator << (std::ostream& out, const Rational& number) { out << number.numerator << "/" << number.denominator; }
---
+++ src/RATIONAL/RATIONAL.h
@@ -0,0 +1,27 @@
+//
+// Created by Mihail Fedorov (2382) on 31.10.2023.
+//
+
+#ifndef DM_FALL_2023_RATIONAL_H
+#define DM_FALL_2023_RATIONAL_H
+
+#include "../Natural/NATURAL.h"
+#include "../Integer/INTEGER.h"
+#include <string>
+#include <algorithm>
+#include <stdexcept>
+
+class Rational {
+public:
+    explicit Rational(const std::string& number);
+    Rational();
+    friend std::ostream& operator << (std::ostream& out, const Rational& number); // Оператор вывода в поток.
+    // Доступ к натуральному числу отсюда или дублировать функции Natural или ваши идеи
+
+private:
+    Integer numerator; // числитель
+    Natural denominator; // знаменатель
+};
+
+
+#endif //DM_FALL_2023_RATIONAL_H
