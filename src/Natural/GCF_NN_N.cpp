89b921bd8b65f66da01aa02fc5bc005d164af939
spanwalla <33371858+spanwalla@users.noreply.github.com>
2023-10-31 09:42:37 +0300

    Update GCF_NN_N.cpp
    format

--- src/Natural/GCF_NN_N.cpp
+++ src/Natural/GCF_NN_N.cpp
@@ -2,15 +2,13 @@
 // Created by Muravin Egor (2382) on 30.10.2023.
 // НОД натуральных чисел
 // 
-//
+
 #include "NATURAL.h"
 
-Natural Natural::GCF_NN_N(const Natural& number)
-{
+Natural Natural::GCF_NN_N(const Natural& number) {
     Natural temp_one = *this;
     Natural temp_two = number;
-    while (temp_one.NZER_N_B() && temp_two.NZER_N_B())
-    {
+    while (temp_one.NZER_N_B() && temp_two.NZER_N_B()) {
         if (temp_one.COM_NN_D(temp_two) == 2)
             temp_one = temp_one.MOD_NN_N(temp_two);
         else
