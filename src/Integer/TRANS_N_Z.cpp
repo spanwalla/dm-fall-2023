2cfc09f39ad0b39f5f696cb0e2cf0d228f7c4a5d
spanwalla <lus550@yandex.ru>
2023-11-01 18:33:08 +0300

    обновил под новую структуру проекта

--- src/Integer/TRANS_N_Z.cpp
+++ src/Integer/TRANS_N_Z.cpp
@@ -2,6 +2,7 @@
 // Преобразование натурального в целое
 // Integer new_integer_obj = Integer::TRANS_N_Z(nat_obj);
 
+#define CLS_EXPORTS
 #include "INTEGER.h"
 
 Integer Integer::TRANS_N_Z(Natural& nat){
