9e9c050f26dc6ac954a366b19ece54981ef8f177
copybarasad <112825887+copybarasad@users.noreply.github.com>
2023-11-01 17:55:50 +0300

    Update INTEGER.h

--- src/Integer/INTEGER.h
+++ src/Integer/INTEGER.h
@@ -20,7 +20,7 @@ public:
     friend std::ostream& operator << (std::ostream& out, const Integer& number); // Оператор вывода в поток.
     // Перегрузить оператор взятия по индексу.
     // Доступ к натуральному числу отсюда или дублировать функции Natural или ваши идеи
-    Natural ABS_Z_N();
+    Integer ABS_Z_N();
 
 private:
     Natural number;
