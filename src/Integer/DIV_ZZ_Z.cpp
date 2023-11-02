0535328f9c503eaa81ea160ba855dcd64b030370
Sorkan176 <ansemenenko176@gmail.com>
2023-11-02 21:23:42 +0300

    DIV_ZZ_Z

--- src/Integer/DIV_ZZ_Z.cpp
+++ src/Integer/DIV_ZZ_Z.cpp
@@ -6,11 +6,11 @@
 
 Integer Integer::DIV_ZZ_Z(Integer& divisor){
     if (!divisor.number.NZER_N_B())
-        throw std::logic_error("The divisor  must not be zero!");
+        throw std::logic_error("The divisor must not be zero!");
     Natural nat = this->number.DIV_NN_N(divisor.number);
     Integer quotient(nat);
     quotient.sign = !(this->sign == divisor.sign || this->POZ_Z_D() == 0);
-    // добавляем единицу по модулю елси делимое - отрицательное с остатком от деления отличным от нуля
+    // добавляем единицу по модулю если делимое - отрицательное с остатком от деления отличным от нуля
     if (this->sign == 1 && this->number.MOD_NN_N(divisor.number).NZER_N_B())
         quotient.number.ADD_1N_N();
     return quotient;
