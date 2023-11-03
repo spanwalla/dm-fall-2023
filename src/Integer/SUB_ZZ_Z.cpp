4128e9c00a241932c791f48726e5e024442fd8d5
spanwalla <33371858+spanwalla@users.noreply.github.com>
2023-11-03 17:32:14 +0300

    Update SUB_ZZ_Z.cpp

--- src/Integer/SUB_ZZ_Z.cpp
+++ src/Integer/SUB_ZZ_Z.cpp
@@ -1,11 +1,11 @@
 // Created by Shegoleva Nadezhda (2382) on 02.11.2023.
-// функция вычитает одно целое число из другого
+// Функция вычитает одно целое число из другого
 
 #define CLS_EXPORTS
 #include "INTEGER.h"
 
 void Integer::SUB_ZZ_Z(Integer b){
-    if (this->sign == b.sign){
+    if (POZ_Z_D() == b.POZ_Z_D()){
         if (this->number.COM_NN_D(b.number) != 1) { // a>=b
             this->number.SUB_NN_N(b.number);
         }
