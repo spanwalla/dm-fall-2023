7ade025c4f8e967fa52daa7c096ba75bbd98d90c
bulterier94 <cantsifst@gmail.com>
2023-11-04 14:41:55 +0300

    Проверка рационального на целое, теперь учитываются случаи 15/3, 10/5 и тд.

---
+++ src/Rational/INT_Q_B.cpp
@@ -0,0 +1,9 @@
+// Created by Bukharin Maxim (2382) on 04.11.2023.
+// Проверка на целое
+
+#include "RATIONAL.h"
+
+bool Rational::INT_Q_B() const {
+    Natural GCF = this->denominator.GCF_NN_N(this->numerator.ABS_Z_Z().TRANS_Z_N()); // находим НОД числителя и знаменателя
+    return this->denominator.COM_NN_D(GCF) == 0; // возвращаем, равен ли НОД знаменателю
+}
