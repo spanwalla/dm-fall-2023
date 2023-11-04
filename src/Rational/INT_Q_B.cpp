bf6111eba8c55b670b394e6cbb39682f72e4892b
bulterier94 <113234278+bulterier94@users.noreply.github.com>
2023-11-04 14:37:27 +0300

    Исправлена ошибка в комментарии

--- src/Rational/INT_Q_B.cpp
+++ src/Rational/INT_Q_B.cpp
@@ -4,10 +4,6 @@
 #include "RATIONAL.h"
 
 bool Rational::INT_Q_B() const {
-<<<<<<< HEAD
-    Natural GCF = this->denominator.GCF_NN_N(this->numerator.ABS_Z_Z().TRANS_Z_N()); // находим НОД делителя и знаменателя
-=======
     Natural GCF = this->denominator.GCF_NN_N(this->numerator.ABS_Z_Z().TRANS_Z_N()); // находим НОД числителя и знаменателя
->>>>>>> 6de5b44 (Проверка рационального на целое, теперь учитываются случаи 15/3, 6/2 и тд.)
     return this->denominator.COM_NN_D(GCF) == 0; // возвращаем, равен ли НОД знаменателю
 }
