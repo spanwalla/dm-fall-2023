86d278b27cac19a72f70803f24ba18bd7e317826
spanwalla <lus550@yandex.ru>
2023-11-04 17:37:47 +0300

    исправления в соответствии с пояснениями условия задачи

--- src/Rational/INT_Q_B.cpp
+++ src/Rational/INT_Q_B.cpp
@@ -1,10 +1,9 @@
 // Created by Bukharin Maxim (2382) on 04.11.2023.
-// Проверка на целое
+// Проверка сокращённого дробного числа на целое.
 
 #define CLS_EXPORTS
 #include "RATIONAL.h"
 
 bool Rational::INT_Q_B() const {
-    Natural GCF = this->denominator.GCF_NN_N(this->numerator.ABS_Z_Z().TRANS_Z_N()); // находим НОД числителя и знаменателя
-    return this->denominator.COM_NN_D(GCF) == 0; // возвращаем, равен ли НОД знаменателю
+    return this->denominator.COM_NN_D(Natural(1)) == 0; // возвращаем, равен ли знаменатель единице
 }
