617fdb131738841e76a4cf4c43962a1b87acbb4d
spanwalla <33371858+spanwalla@users.noreply.github.com>
2023-11-04 17:47:07 +0300

    Update TRANS_Q_Z.cpp
    вернул к первоначальной версии

--- src/Rational/TRANS_Q_Z.cpp
+++ src/Rational/TRANS_Q_Z.cpp
@@ -1,6 +1,6 @@
-// Created by Bukharin Maxim (2382) on 04.11.2023.
-// Преобразование дробного в целое (если знаменатель равен 1)
-// Код может быть переделан с использованием RED_Q_Q, когда будет написан этот метод
+// Created by Maxim Bukharin (2382) on 04.11.2023.
+// Преобразование дробного в целое (если знаменатель равен 1).
+// Код может быть переделан с использованием RED_Q_Q, когда будет написан этот метод.
 
 #define CLS_EXPORTS
 #include "RATIONAL.h"
@@ -8,6 +8,5 @@
 Integer Rational::TRANS_Q_Z() const {
     if (!this->INT_Q_B())
         throw std::invalid_argument("The denominator must be 1.");
-    Natural GCF = this->denominator.GCF_NN_N(this->numerator.ABS_Z_Z().TRANS_Z_N()); // находим НОД числителя и знаменателя
-    return this->numerator.DIV_ZZ_Z(Integer(GCF)); // возвращаем частное числителя и НОДа
+    return this->numerator; // возвращаем числитель
 }
