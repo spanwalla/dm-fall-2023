8a8f04696d4e1014efcc4d69b35cb8cfedf253fe
spanwalla <33371858+spanwalla@users.noreply.github.com>
2023-11-11 00:47:29 +0300

    Update DEP_P_P.cpp

--- src/Polynomial/DEP_P_P.cpp
+++ src/Polynomial/DEP_P_P.cpp
@@ -1,11 +1,11 @@
-// Created by Bukharin Maxim (2382) on 09.11.2023.
-// Производная от многочлена
+// Created by Maxim Bukharin (2382) on 09.11.2023.
+// Производная от многочлена.
 
 #define CLS_EXPORTS
 #include "POLYNOMIAL.h"
 
 Polynomial Polynomial::DEP_P_P() const {
-    std::vector<Rational> new_coefficients(this->DEG_P_N() > 0 ? this->DEG_P_N : 1, Rational("0")); // коэффиценты производного многочлена
+    std::vector<Rational> new_coefficients(this->DEG_P_N() > 0 ? this->DEG_P_N() : 1, Rational("0")); // коэффиценты производного многочлена
     size_t i;
     for (i = 1; i < this->DEG_P_N(); ++i) { // цикл, проходящий по всем индексам вектора коэффицентов многочлена
         new_coefficients[i - 1] = this->coefficients[i];
