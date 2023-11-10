3772eb72161c8980a65ecc494273807f9861e1ec
spanwalla <33371858+spanwalla@users.noreply.github.com>
2023-11-11 01:08:28 +0300

    Update DEP_P_P.cpp

--- src/Polynomial/DEP_P_P.cpp
+++ src/Polynomial/DEP_P_P.cpp
@@ -7,7 +7,7 @@
 Polynomial Polynomial::DEP_P_P() const {
     std::vector<Rational> new_coefficients(this->DEG_P_N() > 0 ? this->DEG_P_N() : 1, Rational("0")); // коэффиценты производного многочлена
     size_t i;
-    for (i = 1; i < this->DEG_P_N(); ++i) { // цикл, проходящий по всем индексам вектора коэффицентов многочлена
+    for (i = 1; i <= this->DEG_P_N(); ++i) { // цикл, проходящий по всем индексам вектора коэффицентов многочлена
         new_coefficients[i - 1] = this->coefficients[i];
         new_coefficients[i - 1].MUL_QQ_Q(Rational(std::to_string(i))); // умножаем текущий коэффицент на значение позиции в векторе, т.е. на степень
     }
