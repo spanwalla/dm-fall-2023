0cd412398f350a35d96cd99ec2488b1ddbf2fad2
spanwalla <33371858+spanwalla@users.noreply.github.com>
2023-11-11 01:24:07 +0300

    Update SUB_PP_P.cpp

--- src/Polynomial/SUB_PP_P.cpp
+++ src/Polynomial/SUB_PP_P.cpp
@@ -1,15 +1,18 @@
 //
 // Created by Mihail Fedorov (2382) on 11.11.2023.
-// вычитание многочленов
+// Вычитание многочленов.
 //
 
 #define CLS_EXPORTS
 #include "POLYNOMIAL.h"
 
 void Polynomial::SUB_PP_P(const Polynomial& other) {
-    for (size_t i = 0; i < other.coefficients.size(); ++i) {
-        if (i >= this->coefficients.size())
-            this->coefficients.push_back(Rational("0"));
-        this->coefficients[i].SUB_QQ_Q(other.coefficients[i]);
+    if (!other.is_zero()) {
+        for (int i = 0; i <= other.DEG_P_N(); ++i) {
+            if (i > this->DEG_P_N())
+                this->coefficients.push_back(Rational("0"));
+            this->coefficients[i].SUB_QQ_Q(other.coefficients[i]);
+        }
+        this->clean_zero();
     }
 }
