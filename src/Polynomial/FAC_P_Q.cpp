be435d947f3118acefd239a103d94569c316650d
mishIgr <fmikhailv@gmail.com>
2023-11-13 00:10:09 +0300

    FAC_P_Q

--- src/Polynomial/FAC_P_Q.cpp
+++ src/Polynomial/FAC_P_Q.cpp
@@ -5,12 +5,14 @@
 #include "POLYNOMIAL.h"
 
 Rational Polynomial::FAC_P_Q() const {
-    Natural numerator = Natural("1");
-    Natural denominator = Natural("1");
+    Natural numerator = this->coefficients[this->coefficients.size() - 1].get_numerator().ABS_Z_Z().TRANS_Z_N();
+    Natural denominator = this->coefficients[this->coefficients.size() - 1].get_denominator();
+    std::cout << "numerator: " << numerator << " " << "denominator: " << denominator << std::endl;
 
-    for (size_t i = 0; i < this->coefficients.size(); ++i) {
+    for (size_t i = 0; i < this->coefficients.size() - 1; ++i) {
         numerator = std::move(numerator.GCF_NN_N(this->coefficients[i].get_numerator().ABS_Z_Z().TRANS_Z_N()));
         denominator = std::move(denominator.LCM_NN_N(this->coefficients[i].get_denominator()));
+        std::cout << "numerator: " << numerator << " " << "denominator: " << denominator << std::endl;
     }
 
     return Rational(Integer::TRANS_N_Z(numerator), denominator);
