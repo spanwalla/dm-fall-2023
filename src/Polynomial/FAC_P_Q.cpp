a744e96350a3585f211abcdae8f1abe2ae90b3e2
spanwalla <33371858+spanwalla@users.noreply.github.com>
2023-11-13 19:01:57 +0300

    Update FAC_P_Q.cpp

--- src/Polynomial/FAC_P_Q.cpp
+++ src/Polynomial/FAC_P_Q.cpp
@@ -1,12 +1,12 @@
-// Created by Dmitry Chepasov (2382) on 11.11.2023.
-// Старший коэффициент многочлена.
+// Created by Mihail Fedorov (2382) on 11.11.2023.
+// Вынесение из многочлена НОД числителей и НОК знаменателей.
 
 #define CLS_EXPORTS
 #include "POLYNOMIAL.h"
 
 Rational Polynomial::FAC_P_Q() const {
-    Natural numerator = this->coefficients[this->coefficients.size() - 1].get_numerator().ABS_Z_Z().TRANS_Z_N();
-    Natural denominator = this->coefficients[this->coefficients.size() - 1].get_denominator();
+    Natural numerator = LED_P_Q().get_numerator().ABS_Z_Z().TRANS_Z_N();
+    Natural denominator = LED_P_Q().get_denominator();
 
     for (size_t i = 0; i < this->coefficients.size() - 1; ++i) {
         numerator = std::move(numerator.GCF_NN_N(this->coefficients[i].get_numerator().ABS_Z_Z().TRANS_Z_N()));
