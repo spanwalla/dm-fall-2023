047a63f78f81c57cf4739cdceafdb7357a6853b4
Copybarasad <kirpichotsutstvuet@gmail.com>
2023-11-22 14:39:05 +0300

    комментарии к DIV_ZZ_Z и MOD_ZZ_Z

--- src/Integer/DIV_ZZ_Z.cpp
+++ src/Integer/DIV_ZZ_Z.cpp
@@ -7,9 +7,9 @@
 Integer Integer::DIV_ZZ_Z(const Integer& divisor) const {
     if (!divisor.number.NZER_N_B())
         throw std::logic_error("The divisor must not be zero!");
-    Natural nat = this->number.DIV_NN_N(divisor.number);
-    Integer quotient(nat);
-    quotient.sign = !(this->sign == divisor.sign || this->POZ_Z_D() == 0);
+    Natural nat = this->number.DIV_NN_N(divisor.number);  // nat это натуральное частное от деления
+    Integer quotient(nat);  // quotient это целое частное от деления
+    quotient.sign = !(this->sign == divisor.sign || this->POZ_Z_D() == 0);  // определяем знак у целого числа
     // добавляем единицу по модулю, если делимое - отрицательное с остатком от деления, отличным от нуля
     if (this->sign == 1 && this->number.MOD_NN_N(divisor.number).NZER_N_B())
         quotient.number.ADD_1N_N();
--- src/Integer/MOD_ZZ_Z.cpp
+++ src/Integer/MOD_ZZ_Z.cpp
@@ -5,13 +5,12 @@
 #include "INTEGER.h"
 
 Integer Integer::MOD_ZZ_Z(const Integer &num) const {
-    Natural nat = this->number.MOD_NN_N(num.number);
-    Integer remainder(nat);
+    Natural nat = this->number.MOD_NN_N(num.number);  // nat это натуральный остаток от деления
+    Integer remainder(nat);  // remainder это целый остаток от деления
     // Если делимое отрицательное и остаток отличен от нуля, то остаток равен (делитель - остаток).
     if (this->sign && remainder.number.NZER_N_B()) {
-        // (остаток минус делитель) по модулю
-        remainder.SUB_ZZ_Z(num.ABS_Z_Z());
-        remainder.sign = false;
+        remainder.SUB_ZZ_Z(num.ABS_Z_Z());  // остаток минус делитель
+        remainder.sign = false;  // остаток положительный
     }
     return remainder;
 }
